5dd6f5f58411181793d01b505060b1fd4dc31b77
Neko-Sato <hisatosi35350@gmail.com>
2024-01-31 04:04:46 +0900

    update

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,13 +6,43 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 03:12:06 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/01/31 03:12:31 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/01/31 04:01:36 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <libft.h>
 #include <stddef.h>
+#include <stdio.h>
+
+int	get_lexical(t_minishell *gvars, char **line, size_t *pos, char **result)
+{
+	size_t	start;
+	int		c;
+	int		ret;
+
+	if (skip_space(gvars, line, pos))
+		return (-1);
+	start = *pos;
+	while (1)
+	{
+		c = pgetc(gvars, line, pos);
+		if (ft_isspace(c) || ft_strchar("<>&|", c))
+			break ;
+		ret = 0;
+		if (c == '\'')
+			ret = skip_singlquote(gvars, line, pos);
+		else if (c == '"')
+			ret = skip_doublequote(gvars, line, pos);
+		else if (c == '\\')
+			ret = skip_backslash(gvars, line, pos);
+		if (ret)
+			return (ret);
+		(*pos)++;
+	}
+	*result = ft_substr(*line, start, *pos - start);
+	return (0);
+}
 
 int	skip_space(t_minishell *gvars, char **line, size_t *pos)
 {
@@ -50,8 +80,13 @@ int	skip_singlquote(t_minishell *gvars, char **line, size_t *pos)
 		c = pgetc(gvars, line, *pos);
 		if (c == -1)
 			return (-1);
-		if (ft_strchr("'", c))
+		if (c == '\'')
 			break ;
+		if (c == '\0')
+		{
+			ft_putendl_fd("minishell: unmatched `'`", STDERR_FILENO);
+			return (1);
+		}
 		(*pos)++;
 	}
 	return (0);
@@ -68,7 +103,12 @@ int	skip_doublequote(t_minishell *gvars, char **line, size_t *pos)
 			return (-1);
 		if (ft_strchr("\"", c))
 			break ;
-		else if (c == '\\')
+		if (c == '\0')
+		{
+			ft_putendl_fd("minishell: unmatched `\"`", STDERR_FILENO);
+			return (1);
+		}
+		if (c == '\\')
 		{
 			(*pos)++;
 			if (skip_backslash(gvars, line, pos))
