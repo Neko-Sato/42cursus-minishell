7169b6db781d6ca5fec0f4dd1999c5abe2655bee
Neko-Sato <hisatosi35350@gmail.com>
2024-03-16 14:14:14 +0900

    update

--- srcs/execute_null_command.c
+++ srcs/execute_null_command.c
@@ -6,35 +6,25 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:05:31 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/13 19:04:36 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/16 11:15:50 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execute.h"
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int	execute_null_command(t_minishell *shell, t_execute vars)
 {
-	int	temp[2];
-
-	while (1)
-	{
-		temp[0] = dup(STDIN_FILENO);
-		if (temp[0] == -1)
-			break ;
-		temp[1] = dup(STDOUT_FILENO);
-		if (temp[1] == -1)
-			break ;
-		if (do_redirect(shell, vars.redirect))
-			break ;
-		if (dup2(temp[0], STDIN_FILENO) == -1)
-			break ;
-		close(temp[0]);
-		if (dup2(temp[1], STDOUT_FILENO) == -1)
-			break ;
-		close(temp[1]);
-		return (0);
-	}
-	return (-1);
+	if (!vars.already_fork && save_stdio(shell))
+		return (-1);
+	if (do_redirect(shell, vars.redirect))
+		return (-1);
+	if (vars.already_fork)
+		exit(EXIT_SUCCESS);
+	if (adapt_stdio(shell))
+		return (-1);
+	return (EXIT_SUCCESS);
 }
