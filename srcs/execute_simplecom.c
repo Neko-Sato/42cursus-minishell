7a9b1df9933c8cd6a74126181b4ba57d43c3dd12
Neko-Sato <hisatosi35350@gmail.com>
2024-03-16 14:15:09 +0900

    update

--- srcs/execute_simplecom.c
+++ srcs/execute_simplecom.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 17:13:45 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/13 18:06:31 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/16 13:11:31 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int	execute_simplecom_finally(t_minishell *shell,
-				t_execute_simplecom *vars);
 static int	execute_simplecom_internal(t_minishell *shell,
 				t_execute_simplecom *vars);
 
@@ -27,6 +25,7 @@ int	execute_simplecom(t_minishell *shell, t_simplecom *simplecom, int pipe_in,
 {
 	pid_t	pid;
 	int		do_fork;
+	int		status;
 
 	do_fork = pipe_in != -1 || pipe_out != -1;
 	if (do_fork)
@@ -41,24 +40,13 @@ int	execute_simplecom(t_minishell *shell, t_simplecom *simplecom, int pipe_in,
 			return (EXIT_SUCCESS);
 		}
 	}
-	return (execute_simplecom_finally(shell, &(t_execute_simplecom){simplecom,
-			pipe_in, pipe_out, do_fork}));
-}
-
-static int	execute_simplecom_finally(t_minishell *shell,
-		t_execute_simplecom *vars)
-{
-	int	status;
-
-	status = execute_simplecom_internal(shell, vars);
-	if (!vars->already_fork)
+	status = execute_simplecom_internal(shell, &(t_execute_simplecom){simplecom,
+			pipe_in, pipe_out, do_fork});
+	if (!do_fork)
 		return (status);
 	if (status == -1)
-	{
 		perror("minishell");
-		status = EXIT_FAILURE;
-	}
-	exit(status);
+	exit(EXIT_FAILURE);
 }
 
 static int	execute_simplecom_internal(t_minishell *shell,
@@ -69,7 +57,8 @@ static int	execute_simplecom_internal(t_minishell *shell,
 
 	if (!vars->simplecom->wordlist)
 		return (execute_null_command(shell, (t_execute){NULL,
-				vars->simplecom->redirect, vars->pipe_in, vars->pipe_out, 0}));
+				vars->simplecom->redirect, vars->pipe_in, vars->pipe_out,
+				vars->already_fork}));
 	wordlist = expand_wordlist(shell, vars->simplecom->wordlist);
 	if (!wordlist)
 		return (-1);
--- srcs/findcmd.c
+++ srcs/findcmd.c
@@ -6,30 +6,54 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 19:26:35 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/12 16:25:30 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/16 12:03:35 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "command.h"
 #include "shell.h"
 #include "variable.h"
+#include <libft.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/*
-	ここではコマンドのpathを検索する
-	コマンドにはもちろん"/"は含まれない
-	PTAHから探す。
+static int	search_for_command_internal(char *path, char *pathname,
+				char **result);
 
-	システムエラー時もnotfoud時もNULLを返します
-	errnoで区別させるのでerrnoを実行前に初期化してください
-*/
-
-char	*search_for_command(t_minishell *shell, char *pathname)
+int	search_for_command(t_minishell *shell, char *pathname, char **result)
 {
-	char *path;
-	char *paths;
+	int		ret;
+	char	*path;
 
+	*result = NULL;
 	path = getvar(shell->envp, "PATH");
 	if (!path)
-		return (NULL);
+		return (0);
+	path = ft_strdup(path);
+	if (!path)
+		return (-1);
+	ret = search_for_command_internal(path, pathname, result);
+	free(path);
+	return (ret);
+}
 
+static int	search_for_command_internal(char *path, char *pathname,
+		char **result)
+{
+	char	*temp;
+
+	while (1)
+	{
+		temp = ft_strsep(&path, ":");
+		if (!temp)
+			break ;
+		temp = ft_joinpath(temp, pathname);
+		if (!temp)
+			return (-1);
+		if (!access(temp, F_OK))
+			break ;
+		free(temp);
+	}
+	*result = temp;
+	return (0);
 }
