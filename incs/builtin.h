0063cff0c10e39db57817aed8d9e2ccec6d69d6e
Neko-Sato <hisatosi35350@gmail.com>
2024-03-19 08:33:11 +0900

    update

--- incs/builtin.h
+++ incs/builtin.h
@@ -6,21 +6,21 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 08:08:12 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/19 08:13:49 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/19 08:32:34 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
-#include "shell.h"
-#include "command.h"
+# include "command.h"
+# include "shell.h"
 
 int	builtin_echo(int argc, char *argv[]);
 int	builtin_env(int argc, char *argv[], char *envp[]);
-int builtin_cd(t_minishell *shell, t_wordlist *list);
-int builtin_pwd(t_minishell *shell, t_wordlist *list);
-int builtin_export(t_minishell *shell, t_wordlist *list);
-int builtin_exit(t_minishell *shell, t_wordlist *list);
+int	builtin_cd(t_minishell *shell, t_wordlist *list);
+int	builtin_pwd(t_minishell *shell, t_wordlist *list);
+int	builtin_export(t_minishell *shell, t_wordlist *list);
+int	builtin_exit(t_minishell *shell, t_wordlist *list);
 
 #endif
