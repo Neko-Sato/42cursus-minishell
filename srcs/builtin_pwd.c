8dfe6b0d47d2637f3c8e8b0024e54d73293f86c1
Neko-Sato <hisatosi35350@gmail.com>
2024-03-19 22:30:16 +0900

    update

--- srcs/builtin_pwd.c
+++ srcs/builtin_pwd.c
@@ -6,17 +6,29 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 18:02:33 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/19 18:33:00 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/19 22:29:19 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include <libft.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int	builtin_pwd(t_minishell *shell, t_wordlist *wordlist)
 {
+	char *current_path;
+
 	(void)shell;
 	(void)wordlist;
-	ft_putendl_fd(wordlist->word, 1);
+	current_path = getcwd(NULL, 0);
+	if (!current_path)
+	{
+		perror("minishell: pwd: ");
+		return (1);
+	}
+	ft_putendl_fd(current_path, 1);
+	free(current_path);
 	return (0);
 }
