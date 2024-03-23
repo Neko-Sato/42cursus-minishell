033fa944f47c6a15fceb55f9760a4e05d4d6129d
Neko-Sato <hisatosi35350@gmail.com>
2024-03-24 00:19:03 +0900

    update exit のエラーメッセージ修正

--- srcs/builtin_exit.c
+++ srcs/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 18:02:24 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/24 00:14:04 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/24 00:18:54 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,41 +15,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-static int	get_status(t_minishell *shell, t_wordlist *wordlist);
+static void	set_last_status(t_minishell *shell, t_wordlist *wordlist);
 
 int	builtin_exit(t_minishell *shell, t_wordlist *wordlist)
 {
 	if (shell->isinteractive)
 		ft_putstr_fd("exit\n", STDERR_FILENO);
-	exit(get_status(shell, wordlist));
+	set_last_status(shell, wordlist);
+	exit(shell->last_status);
 	return (EXIT_FAILURE);
 }
 
-static int	get_status(t_minishell *shell, t_wordlist *wordlist)
+static void	set_last_status(t_minishell *shell, t_wordlist *wordlist)
 {
-	int	status;
-
 	if (wordlist)
 	{
 		if (ft_isnumber(wordlist->word, 1))
 		{
-			status = ft_atoi(wordlist->word);
 			if (wordlist->next)
 			{
 				ft_putstr_fd("minishell: exit: too many arguments\n",
 					STDERR_FILENO);
-				status = EXIT_FAILURE;
+				shell->last_status = EXIT_FAILURE;
 			}
+			else
+				shell->last_status = ft_atoi(wordlist->word);
 		}
 		else
 		{
 			ft_putstr_fd("minishell: exit: numeric argument required: ",
 				STDERR_FILENO);
 			ft_putendl_fd(wordlist->word, STDERR_FILENO);
-			status = 2;
+			shell->last_status = 2;
 		}
 	}
-	else
-		status = shell->last_status;
-	return (status);
 }
