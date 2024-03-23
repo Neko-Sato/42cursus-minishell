481d7555482626c48d265edeae085cddca4e7a27
Neko-Sato <hisatosi35350@gmail.com>
2024-03-24 00:23:23 +0900

    update perrorの位置修正

--- srcs/shell.c
+++ srcs/shell.c
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 00:25:38 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/03/23 16:14:37 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/03/24 00:20:38 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,10 +56,7 @@ static int	reader_loop_internal(t_minishell *shell)
 		shell->heredoc = NULL;
 		cleanup_dead_jobs(shell);
 		if (ret == SYSTEM_ERR)
-		{
-			perror("minishell: ");
-			return (EXIT_FAILURE);
-		}
+			return (-1);
 		else if (!shell->isinteractive && ret == SYNTAX_ERR)
 			break ;
 	}
